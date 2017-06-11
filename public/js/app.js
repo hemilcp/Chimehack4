3728e814baafdcbf7519dd94bb7573244c9f5573
Egor Vinogradov <egorvinogradov@live.ru>
2017-06-11 11:56:57 -0700

    Update demo

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,19 @@
+var API_SERVER = 'https://nameless-shore-34478.herokuapp.com';
+
 angular.module('gt', [])
   .controller('Chat', function($scope){
 
     $scope.messages = [
       {
         text: 'Highly motivated team-builder and team-player. Passionate about front-end development.',
-        user: 'user-1'
+        user: 'user-2'
       },
       {
         text: 'team-builder and team-player. Passionate about front-end development.'
       },
       {
         text: 'Passionate about front-end development.',
-        user: 'user-1'
+        user: 'user-2'
       },
       {
         text: 'Highly motivated team-builder and team-player. Passionate about front-end development.'
@@ -31,12 +33,14 @@ angular.module('gt', [])
 
   .controller('Search', function($scope, $http){
 
+    $scope.isMentors = true;
+
     $http({
-      url: '/users',
+      url: API_SERVER + '/users',
       method: 'GET'
     }).then(function successCallback(response){
         $scope.mentors = response.data.filter(function(item){
-          return item.mentor;
+          return item.mentor === 'true';
         });
         $scope.peers = response.data.filter(function(item){
           return item.mentor === 'false';
